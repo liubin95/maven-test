e125f3983ba8317738fa344c856f3e3e84885690
刘斌 <liu_bin4835@outlook.com>
2022-03-14 13:35:12 +0800

    线程屏障和信号量

--- java-native/src/main/java/SynergeticProcess.java
+++ java-native/src/main/java/SynergeticProcess.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * SynergeticProcess.
  *
- * <p>notify 和 wait 的使用方法，两个线程交替打印
+ * <p>协程，多个线程之间协同工作
  *
  * @author 刘斌
  * @version 0.0.1
@@ -16,15 +21,88 @@ import lombok.extern.slf4j.Slf4j;
 public class SynergeticProcess {
 
   private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(10);
+  private static final Semaphore SEMAPHORE = new Semaphore(1, true);
   private static int I = 0;
 
   public static void main(String[] args) throws InterruptedException {
-    log.info("THREAD1 {}", THREAD1.getState());
+    waitAndNotify();
+    withSemaphore();
+    withCyclicBarrier();
+    COUNT_DOWN_LATCH.await();
+    System.exit(0);
+  }
+
+  /**
+   * 使用 wait 和 Notify 方法，交替打印数字
+   *
+   * @throws InterruptedException Exception
+   */
+  private static void waitAndNotify() throws InterruptedException {
+    log.debug("THREAD1 {}", THREAD1.getState());
     THREAD1.start();
     THREAD2.start();
     COUNT_DOWN_LATCH.await();
-    log.info("THREAD1 {}", THREAD1.getState());
-    System.exit(0);
+    log.debug("THREAD1 {}", THREAD1.getState());
+  }
+
+  /** 使用Semaphore 信号量控制，两个线程交替打印 */
+  private static void withSemaphore() {
+    new Thread(new RunnableWithSemaphore()).start();
+    new Thread(new RunnableWithSemaphore()).start();
+  }
+
+  /** */
+  private static void withCyclicBarrier() {
+    final int[] ints = new int[10000 * 10000];
+    final int threadNum = 5;
+    final int[] res = new int[threadNum];
+    // 并行设置数组
+    Arrays.parallelSetAll(ints, (i) -> new Random().nextInt());
+    log.info("sum stream:{}", Arrays.stream(ints).sum());
+    int sliceLength = ints.length / threadNum;
+    // 线程屏障
+    final CyclicBarrier barrier =
+        new CyclicBarrier(
+            threadNum,
+            // 所有线程到达屏障
+            () -> {
+              final int sum = Arrays.stream(res).sum();
+              log.info("sum:{}", sum);
+            });
+    for (int i = 0; i < threadNum; i++) {
+      int[] slice = Arrays.copyOfRange(ints, i * sliceLength, (i + 1) * sliceLength);
+      int finalI = i;
+      new Thread(
+              () -> {
+                res[finalI] = Arrays.stream(slice).sum();
+                try {
+                  // 单个线程完成
+                  barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                  e.printStackTrace();
+                }
+              })
+          .start();
+    }
+  }
+
+  static class RunnableWithSemaphore implements Runnable {
+
+    @Override
+    public void run() {
+      //noinspection InfiniteLoopStatement
+      while (true) {
+        try {
+          SEMAPHORE.acquire();
+          log.info("{} {}", I++, Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          COUNT_DOWN_LATCH.countDown();
+          SEMAPHORE.release();
+        }
+      }
+    }
   }
 
   private static final Thread THREAD1 =
@@ -32,7 +110,7 @@ public class SynergeticProcess {
           new Runnable() {
             @Override
             public void run() {
-              while (COUNT_DOWN_LATCH.getCount() > 0) {
+              while (true) {
                 try {
                   log.info("{} {}", I++, Thread.currentThread().getName());
                   synchronized (THREAD2) {
@@ -40,7 +118,7 @@ public class SynergeticProcess {
                   }
                   synchronized (THREAD1) {
                     COUNT_DOWN_LATCH.countDown();
-                    log.info("THREAD1 {}", THREAD1.getState());
+                    log.debug("THREAD1 {}", THREAD1.getState());
                     THREAD1.wait();
                   }
                 } catch (InterruptedException e) {
@@ -56,11 +134,11 @@ public class SynergeticProcess {
           new Runnable() {
             @Override
             public void run() {
-              while (COUNT_DOWN_LATCH.getCount() > 0) {
+              while (true) {
                 try {
                   log.info("{} {}", I++, Thread.currentThread().getName());
                   synchronized (THREAD1) {
-                    log.info("THREAD1 {}", THREAD1.getState());
+                    log.debug("THREAD1 {}", THREAD1.getState());
                     THREAD1.notify();
                   }
                   synchronized (THREAD2) {
